16e4cdc624ce028729f76cef8f97341130118eac
ankit-119 <ankit.kaushik066@gmail.com>
2018-09-20 09:30:48 +0530

    assignement for regular expression

--- regular expression/ques1.py
+++ regular expression/ques1.py
@@ -1,4 +1,4 @@
 import re
 string="asdfgdhefjmdsdfvg ankitkaushik066@gmail.com asdfgfhndbgvcxhj ankitkaushik067@gmail.com"
-res=re.findall(r'[A-Za-z]\w*@\w+[.]\w+',string)
+res=re.findall(r'[A-Za-z]\w*@\w+[.]com',string)
 print(res)
